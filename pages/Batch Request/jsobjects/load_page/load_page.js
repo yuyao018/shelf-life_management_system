62e5329aad79093072c7269c693156b92e1b86b6
Yao Yu Chook <chookyaoyu18@gmail.com>
2025-05-19 02:32:40 +0000

    Fix: Reset table selection on page load

--- pages/Batch Request/jsobjects/load_page/load_page.js
+++ pages/Batch Request/jsobjects/load_page/load_page.js
@@ -1,10 +1,15 @@
 export default {
-	manage_batch_buttononClick () {
+	async manage_batch_buttononClick () {
+		await resetWidget("approval_status_select");
+		await resetWidget("status_select");
+		
 		const role = appsmith.store.user_role;
     if (role === "admin" || role === "tester") {
-      storeValue("view_part", "all_request");
+      await storeValue("view_part", "all_request");
+			get_all_data_admin.run();
     } else if (role === "owner") {
-      storeValue("view_part", "my_request");
+      await storeValue("view_part", "my_request");
+			get_all_data_user.run();
     }
 		navigateTo("Batch Request")
 	}
