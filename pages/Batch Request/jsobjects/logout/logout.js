d2be5c880059821a64b5a904231bda931f56f2a7
Yao Yu Chook <chookyaoyu18@gmail.com>
2025-05-19 02:41:48 +0000

    Update: Clear 'user_id' on logout to improve session handling

--- pages/Batch Request/jsobjects/logout/logout.js
+++ pages/Batch Request/jsobjects/logout/logout.js
@@ -3,6 +3,7 @@ export default {
 		removeValue('name');
 		removeValue('email');
   	removeValue('user_role');
+		removeValue('user_id')
   	navigateTo('Login', { replace: true });
 		showAlert("Logout Successful!", "success");
 	}
